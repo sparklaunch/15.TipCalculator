2ffce62a98e9c2c9715419fcee523557b7a57151
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-26 14:37:08 +0900

    Add the title

--- tip-calculator/src/App.svelte
+++ tip-calculator/src/App.svelte
@@ -1,1 +1,23 @@
-<div id="app" />
+<div id="app">
+    <div id="container">
+        <div id="title">
+            <h1>SPLI</h1>
+            <h1>TTER</h1>
+        </div>
+        <div id="card" />
+    </div>
+</div>
+
+<style>
+    #app {
+        display: flex;
+        height: 100%;
+        justify-content: center;
+        align-items: center;
+    }
+    #title > h1 {
+        color: rgb(54, 88, 90);
+        letter-spacing: 8px;
+        font-size: 24px;
+    }
+</style>
